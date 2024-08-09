6536fb251e5c23848f007523db8266eac87387db
Carlos Acosta <carlos428.ca@gmail.com>
2024-08-08 19:34:01 -0500

    Starting the app

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Employees from './components/Employees.vue';
+</script>
+
+<template>
+  <div>
+    <Employees />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ client/src/components/Employees.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Employees</h1>
+  </div>
+</template>
+
+<style scoped>
+.title-input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    background-color: #f8f8f8;
+    color: #111;
+    border-radius: 4px;
+    resize: vertical;
+}
+
+.body-input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    background-color: #f8f8f8;
+    color: #111;
+    border-radius: 4px;
+    resize: vertical;
+}
+</style>
