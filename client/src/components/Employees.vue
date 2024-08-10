55ff5f412cf57b23e6a82ee82ad4dafb41b1ef76
Carlos Acosta <carlos428.ca@gmail.com>
2024-08-09 20:08:02 -0500

    Create the Employees vue component and testing the api call

--- client/src/components/Employees.vue
+++ client/src/components/Employees.vue
@@ -1,8 +1,52 @@
-<script setup></script>
+<script setup>
+  import { ref, onMounted } from 'vue'
+
+  const employees = ref([])
+
+  const name = ref('')
+  const employee_id = ref(0)
+  const isEditing = ref(false)
+  const API_URL = 'http://localhost:3000/employees'
+
+
+  const createEmployee = async() => {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name.value
+      })
+    })
+
+    const data = await response.json()
+
+    employees.value.push(data)
+    name.value = ''
+    employee_id.value = 0
+  }
+
+  const updateEmployee = async() => {
+    console.log('Update employee')
+  }
+
+  const cancelEdit = () => {
+    console.log('Cancel edit')
+  }
+</script>
 
 <template>
   <div>
     <h1>Employees</h1>
+    <input class="title-input" type="text" placeholder="Employee Name" v-model="name" />
+
+    <!-- Only render if editing post -->
+     <button v-if="isEditing" @click="updateEmployee">Update</button>
+     <button v-if="isEditing" @click="cancelEdit">Cancel</button>
+
+    <!-- Only render if not editing post -->
+    <button v-else @click="createEmployee">Create</button>
   </div>
 </template>
 
